d62d5d2431c207bfd27d6802485803cf37513fb9
Mkhotamirais <mkhotamirais@gmail.com>
2024-05-27 23:25:27 +0700

    lupa ganti server

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: `http://localhost:3000/api/warungota`,
-    // baseUrl: `https://warung-ota-server.vercel.app/api/warungota`,
+    // baseUrl: `http://localhost:3000/api/warungota`,
+    baseUrl: `https://warung-ota-server.vercel.app/api/warungota`,
     credentials: "include",
   }),
   tagTypes: ["User", "Product", "Category", "Tag"],
